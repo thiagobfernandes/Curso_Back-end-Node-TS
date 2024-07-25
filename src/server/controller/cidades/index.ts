ef15c71342784360ec462a3fdcfd35e1d704dc46
thiagobfernandes <thiago5437br@gmail.com>
2024-07-25 13:23:31 -0300

    Exportando Dados

--- src/server/controller/cidades/index.ts
+++ src/server/controller/cidades/index.ts
@@ -1,7 +1,17 @@
 import *  as  create from './Create'
+import *  as  GetAll from './GetAll'
+import *  as  getbyid from './Getbyid'
+import *  as  updateById from './UpdateByid'
+import *  as  DeleteByid from './DeleteByid'
+
 
 export const cidadesController = {
     ...create,
+    ...GetAll,
+    ...getbyid,
+    ...updateById,
+    ...DeleteByid
 }
 
-cidadesController.create // aqui estou exportando os dados do Create
+cidadesController.create;
+cidadesController.createValidation; // aqui estou exportando os dados do Create
