2d62d66b78255f8f6e111562c672669c647ba05f
thiagobfernandes <thiago5437br@gmail.com>
2024-07-25 13:21:30 -0300

    Metodo Get

---
+++ src/server/controller/cidades/Getbyid.ts
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/ban-types */
+import { Request, RequestHandler, Response, query } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from 'yup';
+import { validation } from "../../shared/middleware";
+
+interface IparamProps{
+    id?: number; // obrigatorio
+ 
+    
+}
+//paginacao
+
+export const getByidvalidation = validation((getschema) => ({
+    params:getschema<IparamProps>(yup.object().shape({ 
+        id: yup.number().integer().required().moreThan(0),  
+       
+    })),
+
+     }));
+    
+
+
+export const getbyId = async (req: Request <IparamProps>, res :Response) => { //em Icidade, estou tipando por que e o terceiro parametro
+
+   
+
+console.log(req.params)
+
+    
+
+
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('nao implementado');
+}
+
+// const bodyValidation: yup.Schema<Icidade> = yup.object().shape({ //usando a biblioteca yup para validação
+//     nome: yup.string().required().min(4), //  aqui estao os dados que serao verificados
+//     estado: yup.string().required().min(3)
+
+// });
+
+// export const createValidation = validation({
+//     body:bodyValidation
+// });
