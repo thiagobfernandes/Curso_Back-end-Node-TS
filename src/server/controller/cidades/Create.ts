08099da819ebc7977e9e755380f1767440ebd5a1
thiagobfernandes <thiago5437br@gmail.com>
2024-07-25 13:22:33 -0300

    Metodo post

--- src/server/controller/cidades/Create.ts
+++ src/server/controller/cidades/Create.ts
@@ -1,44 +1,48 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/ban-types */
-import { Request, Response } from "express";
+import { Request, RequestHandler, Response, query } from "express";
 import { StatusCodes } from "http-status-codes";
 import * as yup from 'yup';
+import { validation } from "../../shared/middleware";
 
 interface Icidade {  // aqui estou tipando os dados, e estruturando eles
 
     nome: String;
-    cep:Number;
     
+   
 } 
-
-const bodyValidation: yup.Schema<Icidade> = yup.object().shape({ //usando a biblioteca yup para validação
-    nome: yup.string().required().min(4), //  aqui estao os dados que serao verificados
-    cep: yup.number().required()
-
-})
+//tipando os dados para os schemas
 
 
+// const bodyValidation: yup.Schema<Icidade> = yup.object().shape({ //usando a biblioteca yup para validação
+//     nome: yup.string().required().min(4), //  aqui estao os dados que serao verificados
+//     estado: yup.string().required().min(3)
 
-export const create = async (req: Request <{}, {}, Icidade>, res :Response) => { //em Icidade, estou tipando por que e o terceiro parametro
+// });
 
-    let validateData: Icidade | undefined = undefined;  // aqui crei uma variavel para armazenar dos dados que serao validados
-try {
-    validateData =  await bodyValidation.validate(req.body); 
-} catch (error) {
+// export const createValidation = validation({
+//     body:bodyValidation
+// });
 
-const yupError = error as yup.ValidationError; // estou verificando  o erro
+export const createValidation = validation((getschema) => ({
+    body:getschema<Icidade>(yup.object().shape({ //usando a biblioteca yup para validação
+        nome: yup.string().required().min(4), //  aqui estao os dados que serao verificados
+       
+    })),
 
-return res.json({erros: { // aqui estu enviando uma mensagem pro front-end sobre o erro, um objeto erro que contem o atributo com a mensagem do erro
-    default:yupError.message,
-}});
- 
-};
+     }));
+    
 
-console.log(validateData);
+
+export const create  = async (req: Request <{}, {}, Icidade>, res :Response) => { //em Icidade, estou tipando por que e o terceiro parametro
+
+   
+
+console.log(req.body);
 
     
 
 
-    return res.send('Criei');
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('nao implementado');
 }
 
