e694a93c157e54146d30599ce049be32d4182ed7
thiagobfernandes <thiago5437br@gmail.com>
2024-07-26 17:20:30 -0300

    Teste de validacao Getall

---
+++ tests/cidades/Getall.test.ts
@@ -0,0 +1,23 @@
+import { StatusCodes } from "http-status-codes";
+import { testserver } from "../jest.setup";
+
+
+describe('Cidade - Getall', () => {
+
+it('pegar todas', async () => {
+   const res1 = await testserver.post('/cidades').send({nome:'Tupassi'});
+   expect(res1.statusCode).toEqual(StatusCodes.CREATED);
+   const res2 = await testserver.post('/cidades').send({nome:'Tupassi123'});
+   expect(res2.statusCode).toEqual(StatusCodes.CREATED);
+
+   const resBuscada = await testserver.get('/cidades').send();
+
+   expect(Number(resBuscada.header['x-total-count'])).toBeGreaterThan(0);
+   expect(resBuscada.statusCode).toEqual(StatusCodes.OK);
+   expect(resBuscada.body.length).toBeGreaterThan(0);
+
+});
+
+
+
+});
