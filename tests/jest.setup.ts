32036cc1d778f72600cbbe43ff7cc9df403184d5
thiagobfernandes <thiago5437br@gmail.com>
2024-08-01 12:12:26 -0300

    Testes

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,8 +1,15 @@
 import supertest from 'supertest';
 //importando supertest
 import { server } from '../src/server/Server';
+import { Knex } from '../src/server/database/knex';
 //importando server
 
+beforeAll( async () => {
+    await Knex.migrate.latest(); // estou aguardando o migrate 
+});
+afterAll(async () => {
+    await Knex.destroy(); 
+})
 
 
 export const testserver= supertest(server); // exportando dados
