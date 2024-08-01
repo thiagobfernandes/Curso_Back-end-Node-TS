cadf0286b98b5aecd85f31d8c3523df496815a30
thiagobfernandes <thiago5437br@gmail.com>
2024-08-01 17:49:29 -0300

    Controller de GetByID

--- src/server/controller/cidades/Getbyid.ts
+++ src/server/controller/cidades/Getbyid.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/ban-types */
+
 import { Request, RequestHandler, Response, query } from "express";
 import { StatusCodes } from "http-status-codes";
 import * as yup from 'yup';
 import { validation } from "../../shared/middleware";
+import { Knex } from "knex";
+import { cidadesProvider } from "../../database/providers/cidades";
 
 interface IparamProps{
     id?: number; // obrigatorio
@@ -24,35 +26,26 @@ export const getByidvalidation = validation((getschema) => ({
 
 export const getbyId = async (req: Request <IparamProps>, res :Response) => { //em Icidade, estou tipando por que e o terceiro parametro
 
+if(!req.params.id){
+    return res.status(StatusCodes.BAD_REQUEST).json({
+        errors:{
+            default:'voce precisa digitar o ID'
+        }
+    })
+}
+
+const result = await cidadesProvider.Getbyid(req.params.id);
+if(result instanceof Error){
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        errors:{
+            default:result.message
+        }
+    })
+}
    
 
-console.log(req.params)
-
-  
-if(Number(req.params.id) === 9999) return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-    errors:{
-        default:'registro nao encontrado'
-    }
-});
-    
-
-
-    return res.status(StatusCodes.OK).json({
-        id: req.params.id,
-        nome:'caxias do sul',
-    });
-    
-
+return res.status(StatusCodes.OK).send(result);
 
    
 }
 
-// const bodyValidation: yup.Schema<Icidade> = yup.object().shape({ //usando a biblioteca yup para validação
-//     nome: yup.string().required().min(4), //  aqui estao os dados que serao verificados
-//     estado: yup.string().required().min(3)
-
-// });
-
-// export const createValidation = validation({
-//     body:bodyValidation
-// });
