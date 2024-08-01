dd14539575d46fd1be6fe2ee44ba83d3533d4679
thiagobfernandes <thiago5437br@gmail.com>
2024-08-01 17:51:17 -0300

    Servidor

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 import {server} from './server/Server';
 import 'dotenv/config';
+import { Knex } from './server/database/knex';
 
+const startServer = () => {
 server.listen(process.env.PORT || 3000, () => {
     console.log(`app rodando na porta ${process.env.PORT || 3000} `);
 });
+};
+if(process.env.IS_LOCALHOST !== 'true'){
+Knex.migrate.latest().then(() => {
+startServer();
+})
+.catch(console.log)
+} else {
+    startServer();
+}
