0f2a147e0f239b1cd2e2068914dd5bb5ffd0b942
thiagobfernandes <thiago5437br@gmail.com>
2024-08-08 16:48:54 -0300

    Controller de cidade pegando pelo id

--- src/server/database/providers/cidades/Getbyid.ts
+++ src/server/database/providers/cidades/Getbyid.ts
@@ -4,7 +4,10 @@ import { ICidade } from "../../models";
 
  export const Getbyid = async (id:number):Promise<ICidade|Error> => {
     try{
-    const result = await Knex(EtableNames.cidades).select('*').where('id', '=', id).first();
+    const result = await Knex(EtableNames.cidades)
+    .select('*')
+    .where('id', '=', id)
+    .first();
     if(result) return result;
     return new Error('registro nao encontrado');
 } catch(error) {
