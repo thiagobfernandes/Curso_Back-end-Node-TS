ce2a424aecdd46f949c8cb94551f3cea68bc8a47
thiagobfernandes <thiago5437br@gmail.com>
2024-08-08 19:04:42 -0300

    routes:

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -4,6 +4,7 @@ import { Router } from "express";
 import {cidadesController} from './../controller';
 import { PessoasController } from "./../controller";
 import { UsuarioController } from "./../controller";
+import {ensureAuthenticated} from '../shared/middleware/index'
 
 
 
@@ -16,20 +17,20 @@ router.post('/cadastrar', UsuarioController.UsuarioValidation, UsuarioController
 
 
 
-router.get('/pessoas', PessoasController.getAllValidation, PessoasController.getAll)
-router.post('/pessoas',PessoasController.CreateValidation, PessoasController.createPessoas)
-router.get('/pessoas/:id', PessoasController.GetByIdValidation, PessoasController.GetbyId)
-router.put('/pessoas/:id',PessoasController.updateValidation, PessoasController.UpdatePessoas)
-router.delete('/pessoas/:id', PessoasController.DeleteByIdValidation, PessoasController.deletebyid);
+router.get('/pessoas', ensureAuthenticated, PessoasController.getAllValidation, PessoasController.getAll)
+router.post('/pessoas', ensureAuthenticated,PessoasController.CreateValidation, PessoasController.createPessoas)
+router.get('/pessoas/:id', ensureAuthenticated, PessoasController.GetByIdValidation, PessoasController.GetbyId)
+router.put('/pessoas/:id', ensureAuthenticated,PessoasController.updateValidation, PessoasController.UpdatePessoas)
+router.delete('/pessoas/:id', ensureAuthenticated, PessoasController.DeleteByIdValidation, PessoasController.deletebyid);
 
 
 
 
-router.get('/cidades', cidadesController.getAllValidation, cidadesController.getAll);
-router.get('/cidades/:id', cidadesController.getByidvalidation, cidadesController.getbyId);
-router.put('/cidades/:id', cidadesController.updateByidValidation, cidadesController.updateById);
-router.delete('/cidades/:id', cidadesController.DeletebyidValidation, cidadesController.DeleteByid);
-router.post('/cidades', cidadesController.createValidation, cidadesController.create);
+router.get('/cidades', ensureAuthenticated, cidadesController.getAllValidation, cidadesController.getAll);
+router.get('/cidades/:id', ensureAuthenticated, cidadesController.getByidvalidation, cidadesController.getbyId);
+router.put('/cidades/:id', ensureAuthenticated, cidadesController.updateByidValidation, cidadesController.updateById);
+router.delete('/cidades/:id', ensureAuthenticated, cidadesController.DeletebyidValidation, cidadesController.DeleteByid);
+router.post('/cidades', ensureAuthenticated, cidadesController.createValidation, cidadesController.create);
     // teste de integracao
     
 
