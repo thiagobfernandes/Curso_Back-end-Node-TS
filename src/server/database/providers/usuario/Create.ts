19399c84ae55aafc4a345492fa8efab46815585b
thiagobfernandes <thiago5437br@gmail.com>
2024-08-08 19:04:27 -0300

    Create

--- src/server/database/providers/usuario/Create.ts
+++ src/server/database/providers/usuario/Create.ts
@@ -1,9 +1,13 @@
 import { EtableNames } from "../../knex/ETableNames";
 import { Iusuario } from "../../models";
 import { Knex } from "../../knex";
+import { PasswordCrypto } from "../../../shared/service";
 
 export const create = async(usuario: Omit<Iusuario, 'id'>): Promise<number | Error> => {
     try{
+        const hashPassword = await PasswordCrypto.hashPassword(usuario.password);
+            usuario.password = hashPassword;
+
         const [result] = await Knex(EtableNames.usuarios).insert(usuario).returning('id');
         if(typeof result === 'object'){
             return result.id;
