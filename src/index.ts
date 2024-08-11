2b35a4ff25d968d08a8a6484cf772c7d1b6bb829
thiagobfernandes <thiago5437br@gmail.com>
2024-08-11 19:14:41 -0300

    alteracoes

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
-import {server} from './server/Server';
-import 'dotenv/config';
 import { Knex } from './server/database/knex';
+import { server } from './server/Server';
+
 
 const startServer = () => {
-server.listen(process.env.PORT || 3000, () => {
-    console.log(`app rodando na porta ${process.env.PORT || 3000} `);
-});
+  server.listen(process.env.PORT || 3333, () => {
+    console.log(`App rodando na porta ${process.env.PORT || 3333}`);
+  });
 };
-if(process.env.IS_LOCALHOST !== 'true'){
-Knex.migrate.latest().then(() => {
-Knex.seed.run().then(() => startServer())
-.catch(console.log);
-})
-.catch(console.log)
+
+
+if (process.env.IS_LOCALHOST !== 'true') {
+  console.log('Rodando migrations');
+
+  Knex.migrate
+    .latest()
+    .then(() => {
+      Knex.seed.run()
+        .then(() => startServer())
+        .catch(console.log);
+    })
+    .catch(console.log);
 } else {
-    startServer();
+  startServer();
 }
--- src/server/database/knex/index.ts
+++ src/server/database/knex/index.ts
@@ -1,20 +1,21 @@
-import knex from "knex";
-import {development, production, test} from './Environment';
-import pg from "pg";
-import 'dotenv/config'
+import { knex } from 'knex';
+import 'dotenv/config';
+import pg from 'pg';
 
-if(process.env.NODE_ENV === 'production'){
-  pg.types.setTypeParser(20, 'text', parseInt)
-    
+import { development, production, test } from './Environment';
+
+
+if (process.env.NODE_ENV === 'production') {
+  pg.types.setTypeParser(20, 'text', parseInt);
 }
 
+const getEnvironment = () => {
+  switch (process.env.NODE_ENV) {
+    case 'production': return production;
+    case 'test': return test;
 
-const getEnviroment = () => { // aqui estou definindo o tipo do ambiente
-    switch(process.env.NODE_ENV){
-        case 'production': return production;
-        case 'test': return test;
-        default: return development
-    } // definindo ambietnes
-}
+    default: return development;
+  }
+};
 
-export const Knex = knex(getEnviroment());
+export const Knex = knex(getEnvironment());
