0012350636ff7cb7f05d9e7b46bd9512c7c42b0d
thiagobfernandes <thiago5437br@gmail.com>
2024-08-11 01:16:23 -0300

    boa

--- src/server/database/knex/Environment.ts
+++ src/server/database/knex/Environment.ts
@@ -1,60 +1,45 @@
-import { Knex } from "knex";
-import path from 'path'
+import { Knex } from 'knex';
+import path from 'path';
 
- // nesse arquivo estou criando um banco de dados local 
-
-export  const development: Knex.Config = {
-    client: 'sqlite3', //banco de dados local,
-    useNullAsDefault:true,
-    connection:{
-        filename:path.resolve(__dirname,'..','..','..','..','database.sqlite'), // caminho aonde vai ser criado o db
+export const development: Knex.Config = {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+        filename: path.resolve(__dirname, '..', '..', '..', '..', 'database.sqlite'),
     },
-    migrations:{
-        directory:path.resolve(__dirname, '..', 'migrations'), 
+    migrations: {
+        directory: path.resolve(__dirname, '..', 'migrations'),
     },
-    seeds:{
-        directory:path.resolve(__dirname, '..', 'seeds'), 
+    seeds: {
+        directory: path.resolve(__dirname, '..', 'seeds'),
     },
     pool: {
-        // eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any
-        afterCreate: (connection: any , done: Function) => {
-            connection.run('PRAGMA foreign_keys = ON'); // aqui estou falanado que o banco de dados pode trabalhar com chaves estrangeiras
+        afterCreate: (connection: any, done: Function) => {
+            connection.run('PRAGMA foreign_keys = ON');
             done();
-            console.log(path.resolve(__dirname,'..','..','..','..', 'database.sqlite'));
-
         }
     }
-   
 };
 
+export const test: Knex.Config = {
+    ...development,
+    connection: ':memory',
+};
 
-
-
-export  const test :Knex.Config = {
-    
-...development,
- connection : ':memory',  // coneccao em memoria, quando termina o teste o banco de dados e apagado
-    };
-
-
-
-
-export const production:Knex.Config = {
-    client:'pg', //banco de dados local,
-    migrations:{
-        directory:path.resolve(__dirname, '..', 'migrations'), 
+export const production: Knex.Config = {
+    client: 'pg',
+    migrations: {
+        directory: path.resolve(__dirname, '..', 'migrations'),
     },
-    seeds:{
-        directory:path.resolve(__dirname, '..', 'seeds'), 
+    seeds: {
+        directory: path.resolve(__dirname, '..', 'seeds'),
     },
-    connection:{
+    connection: {
         host: process.env.DATABASE_HOST,
         user: process.env.DATABASE_USER,
-        database:process.env.DATABASE_NAME,
+        database: process.env.DATABASE_NAME,
         password: process.env.DATABASE_PASSWORD,
         port: Number(process.env.DATABASE_PORT),
-        ssl: {rejectUnauthorized:false},
-    
+        ssl: { rejectUnauthorized: false },
     }
-    
 };
