7d79c9164bf2f28d5366d4dfc246db8851a6a216
thiagobfernandes <thiago5437br@gmail.com>
2024-08-12 13:48:01 -0300

    configurando CORS

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import {router} from './routes';
+import cors from 'cors';
+
 
 
 const server=express();
 
+server.use(cors( {
+    origin:process.env.ENABLED_CORS?.split(';') || [] // isso e para permitir enderecos front-end para acessar nossa aplicacao
+}));
 server.use(express.json());
 server.use(router);
 
